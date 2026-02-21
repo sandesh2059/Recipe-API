0a120c448aed579600d0422be6009d9f4a7e4ba1
Sandesh <sandeshchy2059@gmail.com>
2026-02-21 21:44:20 +0545

    recipe model created

--- Recipe/flavor/models.py
+++ Recipe/flavor/models.py
@@ -13,3 +13,17 @@ class Ingredient(models.Model):
     
     def __str__(self):
         return self.name
+    
+class Recipe(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    instructions = models.TextField(blank=True)
+    ingredients = models.ManyToManyField(Ingredient, related_name='recipes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+    
+
